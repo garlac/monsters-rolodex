cf19c4c54763236a70cfcb5e9c7bede51c5c443b
Chandrakanth <chandrakanth@chandrakanth-ubuntu-18.04LTS>
2020-01-10 08:02:38 +0530

    Updates

---
+++ precache-manifest.98a687eb6bae734a290865f56e666f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55639a86863e8c79306b25f668188a3b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f04d9caa351f16d1578",
+    "url": "/monsters-rolodex/static/css/main.4e14f41b.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f04d9caa351f16d1578",
+    "url": "/monsters-rolodex/static/js/main.a6020e60.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
